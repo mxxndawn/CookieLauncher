c2727c0f8513e5fd1d7d4835d6b3bc4741d6a157
mxxndawn <moondawn2112@gmail.com>
2025-02-02 05:45:35 +0900

    Change resolution default to 1920x1080

--- app/assets/js/configmanager.js
+++ app/assets/js/configmanager.js
@@ -78,8 +78,8 @@ function resolveSelectedRAM(ram) {
 const DEFAULT_CONFIG = {
     settings: {
         game: {
-            resWidth: 1280,
-            resHeight: 720,
+            resWidth: 1920,
+            resHeight: 1080,
             fullscreen: false,
             autoConnect: true,
             launchDetached: true
