d0e2137ea6549cb8280499a539b4a20083d6787e
mxxndawn <moondawn2112@gmail.com>
2025-02-02 07:00:00 +0900

    Modify some text content

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,6 +1,5 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
-// SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
 exports.AZURE_CLIENT_ID = '0f5b226f-33b8-4d10-b92b-266a8d9ba3aa'
 // SEE NOTE ABOVE.
 
